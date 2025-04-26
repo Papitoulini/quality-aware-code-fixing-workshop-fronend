fcf62427e606b457cd28037508192cb1f76cc761
Thomas Karanikiotis <tkaranikio@gmail.com>
2025-04-26 21:59:38 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,6 @@ module.exports = {
   apps : [{
     name   : "workshop-frontend",
 		script: "npm",
-		args: ["run", "serve"],
-		instances: 1,
-		exec_mode: "cluster",
+		args: ["run", "dev"],
   }]
 }
